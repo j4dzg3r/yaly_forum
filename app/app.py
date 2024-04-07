1ef9b2c2981d71516cf24e6b26243d759d4dd27e
Artem Novikov <artnov1kov@yandex.ru>
2024-04-07 19:29:22 +0300

    da

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_restful import reqparse, abort, Api, Resource
+
+from data import db_session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "dfaasdjkfajsdkfjaklsdhjklfasjhdk"
+api = Api(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main() -> None:
+    db_session.global_init("yaly.sqlite")
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
+
