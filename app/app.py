f473e6440724e5e63d73e069e9c31b910bf95c5e
Artem Novikov <artnov1kov@yandex.ru>
2024-04-07 21:07:49 +0300

    da

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ login_manager.init_app(app)
 
 
 def main() -> None:
-    db_session.global_init("db/yaly.sqlite")
+    db_session.global_init("../db/yaly.sqlite")
     app.run(host="0.0.0.0", port=8080, debug=True)
 
 
