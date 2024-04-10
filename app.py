c7133551a0915dd298b68d4d3738b922c4f21386
GrygoryZach <grishazschepilo@gmail.com>
2024-04-10 16:36:42 +0200

    -Reading article's pure wiki-markdown text by /wiki/<title> -Editing/creating article by /wiki/<title>?&action=edit -Non-existent articles are opened as a template with a hyperlink to creating the article.
    todo: parse the wiki-markdown, create better templates)

--- app.py
+++ app.py
@@ -20,40 +20,51 @@ api = Api(app)
 # login_manager.init_app(app)
 
 
-@app.route('/article/<string:title>', methods=['GET', 'POST'])
-def make_revision(title):
+@app.route('/wiki/<string:title>', methods=['GET', 'POST'])
+def article(title):
     form = RevisionForm()
+    is_editing = request.args.get('action') == "edit"
+    oldid = request.args.get('oldid')
     db_sess = db_session.create_session()
-    is_new = title not in map(lambda x: x.title, list(db_sess.query(Article).all()))
-    if form.validate_on_submit():
-        article = Article(
-            title=title
-        )
+    article_exist = (title,) in list(db_sess.query(Article.title).all())
+    if article_exist:
+        article = db_sess.query(Article).filter(Article.title == title).first()
+        if request.method == "GET" and article_exist:
+                form.content.data = article.revisions[-1].markdown_content
+    if is_editing:
+        if form.validate_on_submit():
+            if not article_exist:
+                article = Article(
+                    title=title
+                )
+                db_sess.add(article)
 
-        revision = Revision(
-            author_id=1,
-            article_id=article.id,
-            created_at=datetime.now(),
-            markdown_content=form.content.data,
-            verified=False
-        )
+            revision = Revision(
+                author_id=1,
+                article_id=article.id,
+                created_at=datetime.now(),
+                markdown_content=form.content.data,
+                verified=False
+            )
 
-        if is_new:
-            db_sess.add(article)
-        else:
+            article.revisions.append(revision)
+            db_sess.merge(article)
+            db_sess.commit()
+            return redirect(f'/wiki/{title}')
+        return render_template('revision.html',
+                               title=[f"Создание страницы «{title}»", f"Редактирование: {title}"][article_exist],
+                               form=form)
+    else:
+        if article_exist:
             article = db_sess.query(Article).filter(Article.title == title).first()
-
-        article.revisions.append(revision)
-        db_sess.merge(article)
-        db_sess.commit()
-        return redirect('/a')
-    return render_template('revision.html',
-                           title=[f"Создание страницы «{title}»", f"Редактирование: {title}"][not is_new],
-                           form=form)
+            markdown_content = db_sess.query(Revision).filter(Revision.article_id == article.id).all()[-1].markdown_content
+            return render_template('article.html', title=title, markdown_content=markdown_content, answer=True)
+        else:
+            return render_template('article.html', title=title, answer=False)
 
 
 def main() -> None:
-    db_session.global_init("db/ yaly.sqlite")
+    db_session.global_init("db/yaly.sqlite")
     app.run(host="0.0.0.0", port=8080, debug=True)
 
 
